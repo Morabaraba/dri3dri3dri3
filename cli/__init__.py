a1c3c25592ad458b1ffc6a09a093c3b8b85c66a0
OpenHazel <c9.io@openhazel.co.za>
2017-11-12 12:45:34 +0000

    basic js minify from cli

--- cli/__init__.py
+++ cli/__init__.py
@@ -3,9 +3,45 @@ import datetime
 import click
 
 from ..core import app
+from ..js import closure_minify_js_app, closure_minify_js_lib
+
 
 @app.cli.command()
-def minify_js(js_dir='static/app/js', out_js_file='static/app/app.min.js'):
+def minify_js_lib():
+    start_time = datetime.datetime.now()
+    click.echo('Minifying js lib...')
+    closure_minify_js_lib()
+    
+    end_time = datetime.datetime.now()
+    total_time = end_time - start_time
+    click.echo('Done minifying js, took %s' % (total_time))
+
+
+@app.cli.command()
+def minify_js_app():
+    start_time = datetime.datetime.now()
+    click.echo('Minifying js app...')
+    closure_minify_js_app()
+    
+    end_time = datetime.datetime.now()
+    total_time = end_time - start_time
+    click.echo('Done minifying js, took %s' % (total_time))
+    
+    
+@app.cli.command()
+def minify_js():
+    '''Minify all `static/app/js/` to `static/app/app.min.js` and `static/app/lib.min.js`'''
+    start_time = datetime.datetime.now()
+    closure_minify_js_lib()
+    closure_minify_js_app()
+    
+    end_time = datetime.datetime.now()
+    total_time = end_time - start_time
+    click.echo('Done minifying js, took %s' % (total_time))
+    
+
+@app.cli.command()
+def minify_css(js_dir='static/app/js', out_js_file='static/app/app.min.js'):
     '''Minify all `static/app/js` into `static/app/app.min.js`'''
     start_time = datetime.datetime.now()
     click.echo('Minifying js "%s" into "%s"...' % (js_dir, out_js_file))
---
+++ js/__init__.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+import closure
+
+def find_js_files(indir):
+	rootdir = Path(indir)
+	# Return a list of regular files only, not directories
+	return [f.relative_to(indir).as_posix() for f in rootdir.glob('**/*.js') if f.is_file()]
+
+def closure_minify_js_app():
+    out_js_file='static/app/app.min.js'
+    js_args= [
+        '--js=static/app/js/app/**.js',
+        '--js_output_file=%s' % (out_js_file),
+    ]
+    closure.run(*js_args)
+
+def closure_minify_js_lib():
+    out_js_file='static/app/lib.min.js'
+    js_lib_dir = 'static/app/js/lib'
+    js_arg_templ =  '--js=%s'
+    js_args = [
+        '--js=static/app/js/lib/firebugx.js',
+        '--js=static/app/js/lib/jquery-1.11.2.min.js',
+        '--js=static/app/js/lib/jquery-ui-1.11.3.min.js',
+        '--js=static/app/js/lib/jquery.event.drag-2.3.0.js',
+        '--js=static/app/js/lib/slickgrid/slick.core.js',
+        '--js=static/app/js/lib/slickgrid/plugins/slick.autotooltips.js',
+        '--js=static/app/js/lib/slickgrid/plugins/slick.cellrangedecorator.js',
+        '--js=static/app/js/lib/slickgrid/plugins/slick.cellrangeselector.js',
+#       '--js=static/app/js/lib/slickgrid/plugins/slick.cellexternalcopymanager.js'
+#       '--js=static/app/js/lib/slickgrid/plugins/slick.cellselectionmodel.js',
+        '--js=static/app/js/lib/slickgrid/slick.editors.js',
+        '--js=static/app/js/lib/slickgrid/slick.formatters.js',
+        '--js=static/app/js/lib/slickgrid/slick.grid.js',
+        '--js=static/app/js/lib/slickgrid/slick.dataview.js',
+        '--js=static/app/js/lib/onmount.v1.3.0.js',
+        '--js=static/app/js/lib/lodash.v3.10.1.custom.min.js',
+        '--js=static/app/js/lib/backbone-v1.3.3.min.js',
+        '--js=static/app/js/lib/goldenlayout-v1.5.9.min.js'
+        ] # hard code specific deps to the top
+        
+    for js_file in find_js_files(js_lib_dir):
+        js_arg = js_arg_templ % (js_lib_dir + '/' + js_file)
+        if js_arg not in js_args:
+            js_args.append(js_arg)
+    js_args = js_args + [
+        '--js_output_file=%s' % (out_js_file),
+    ]
+    closure.run(*js_args)
